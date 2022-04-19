c13de485dddd25c58021a992a9960ea1381a0095
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-04-19 19:00:24 +0300

    контент

---
+++ components/Layout/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content">
+    <FadeTransition :duration="200" mode="out-in">
+      <!-- контент будет тут -->
+      <nuxt></nuxt>
+    </FadeTransition>
+  </div>
+</template>
+<script>
+import { FadeTransition } from 'vue2-transitions';
+export default {
+  components: {
+    FadeTransition
+  }
+};
+</script>
+<style></style>
