c6c7a9fb3727fba0e328bbd10a7382eaa6cabe9d
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-04-19 18:25:05 +0300

    content:footer

---
+++ components/Layout/ContentFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <ul class="nav">
+        <li class="nav-item">
+          <router-link to="/terms" class="text-info"><i class="tim-icons icon-notes"></i> Terms of Use</router-link>
+        </li>
+      </ul>
+      <div class="copyright">
+        &copy; 2017 - {{ new Date().getFullYear() }} SmartHoldem Platform <small>v.{{version}}</small>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+import packageJson from '../../package.json'
+export default {
+  data() {
+    return {
+      version: packageJson.version,
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
