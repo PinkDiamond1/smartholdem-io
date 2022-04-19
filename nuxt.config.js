f611ce6748d01c407787c6320b7bdee99409fb71
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2022-04-19 18:49:57 +0300

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
     ** Headers of the page
     */
     head: {
-        title: 'XBTS DEX',
+        title: 'SmartHoldem Platform',
         meta: [
             {charset: 'utf-8'},
             {name: 'viewport', content: 'width=device-width, initial-scale=1'},
